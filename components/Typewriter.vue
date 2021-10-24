16b9844d30d134516194ddd53f2b2dfa5cc0793a
Bruno Rodrigues <github@brunodsr.com>
2021-10-24 17:11:56 -0300

    [PORTFOLIO#2] - Start with nuxt 3

--- components/Typewriter.vue
+++ components/Typewriter.vue
@@ -1,5 +1,5 @@
 <template>
-  <p class="text-xl md:text-3xl mt-16 text-purple-500">
+    <p class="text-xl md:text-3xl mt-16 text-purple-500">
     <span> {{ typeValue }}</span
     ><span
       class="cursor inline-block m-0.5 w-0.5 bg-purple-500"
@@ -9,46 +9,34 @@
   </p>
 </template>
 
-<script>
+<script lang="ts">
 export default {
-  props: {
-    typeArray: {
-      type: Array,
-      required: true,
+    setup() {
+        const typeValue = ''
+        const typeStatus = false
+        const typingSpeed = 100
+        const erasingSpeed = 50
+        const newTextDelay = 2000
+        const typeArrayIndex = 0
+        const charIndex = 0
     },
-  },
-  data: () => ({
-    typeValue: '',
-    typeStatus: false,
-    typingSpeed: 100,
-    erasingSpeed: 50,
-    newTextDelay: 2000,
-    typeArrayIndex: 0,
-    charIndex: 0,
-  }),
-  created() {
-    setTimeout(this.typeText, this.newTextDelay + 200)
-  },
-  methods: {
-    typeText() {
-      if (this.charIndex < this.typeArray[this.typeArrayIndex].length) {
-        if (!this.typeStatus) this.typeStatus = true
-
-        this.typeValue += this.typeArray[this.typeArrayIndex].charAt(
-          this.charIndex
-        )
-        this.charIndex += 1
-
-        setTimeout(this.typeText, this.typingSpeed)
-      } else {
-        this.typeStatus = false
-        setTimeout(this.eraseText, this.newTextDelay)
-      }
-    },
+    methods: {
+        typeText() {
+            if (this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+                if (!this.typeStatus) this.typeStatus = true
+                this.typeValue += this.typeArray[this.typeArrayIndex].charAt(
+                    this.charIndex
+                )
+                this.charIndex += 1
+                setTimeout(this.typeText, this.typingSpeed)
+            } else {
+                this.typeStatus = false
+                setTimeout(this.eraseText, this.newTextDelay)
+            }
+            },
     eraseText() {
       if (this.charIndex > 0) {
         if (!this.typeStatus) this.typeStatus = true
-
         this.typeValue = this.typeArray[this.typeArrayIndex].substring(
           0,
           this.charIndex - 1
@@ -63,28 +51,6 @@ export default {
         setTimeout(this.typeText, this.typingSpeed + 1000)
       }
     },
-  },
+    }
 }
 </script>
-
-<style scoped>
-p span.cursor {
-  animation: cursorBlink 1s infinite;
-}
-
-p span.typing {
-  animation: none;
-}
-
-@keyframes cursorBlink {
-  49% {
-    background-color: #8b5cf6;
-  }
-  50% {
-    background-color: transparent;
-  }
-  99% {
-    background-color: transparent;
-  }
-}
-</style>
--- layouts/default.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="bg-gray-900 min-h-screen">
-    <vue-particles color="#B968C7" class="fixed w-full h-full z-0" />
-    <div class="z-10">
-      <Nuxt />
-    </div>
-  </div>
-</template>
