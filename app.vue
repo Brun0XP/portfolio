ff16840f8c6c14d259e56ccf0cb3791e582a0945
Bruno Rodrigues <github@brunodsr.com>
2023-03-10 22:05:51 -0300

    Nova versao

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <header class="bg-gray-200">
+      <div class="flex justify-between py-4 max-w-6xl mx-auto">
+        <span>BR</span>
+        <nav>
+          <ul class="flex space-x-4">
+            <li>ABOUT</li>
+            <li>SKILLS</li>
+            <li>PORTFOLIO</li>
+            <li>CONTACT</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="max-w-6xl mx-auto">
+    <h1 class="text-center text-6xl mt-16"><span class="text-2xl">HELLO, I`M</span><br />BRUNO RODRIGUES</h1>
+    <h2 class="text-center text-2xl text-blue-600">FULLSTACK DEVELOPER</h2>
+  </div>
+</template>
